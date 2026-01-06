31e169a6766a70a8c18d3e441af953ce78fdea53
Digamber Fuckner 👺 <ro46436mi@gmail.com>
2026-01-06 21:01:58 +0530

    Create index.ts

---
+++ src/bot/events/index.ts
@@ -0,0 +1,13 @@
+import * as ready from './ready';
+import * as messageCreate from './messageCreate';
+import * as interactionCreate from './interactionCreate';
+import * as guildMemberAdd from './guildMemberAdd';
+import * as guildMemberUpdate from './guildMemberUpdate';
+
+export {
+  ready,
+  messageCreate,
+  interactionCreate,
+  guildMemberAdd,
+  guildMemberUpdate
+};
